dde1763b437a787e136e6b5730ee75fac634e8fd
Kirsten Jones <kirstenjones@yahoo.co.uk>
2015-06-24 09:35:03 +0100

    progress

--- lib/city_temperature_reporter.rb
+++ lib/city_temperature_reporter.rb
@@ -1,4 +1,12 @@
-class TemperatureReporter
+class CityTemperatureReporter
+  attr_reader :city_name, :weatherman
 
+  def initialize city_name, weatherman
+    @city_name  = city_name
+    @weatherman = weatherman
+  end
 
+  def request_temperature
+    weatherman.get_temperature('New_York')
+  end
 end
--- spec/city_temperature_reporter_spec.rb
+++ spec/city_temperature_reporter_spec.rb
@@ -1,13 +1,32 @@
 require './lib/city_temperature_reporter'
 
-describe TemperatureReporter do
+describe CityTemperatureReporter do
+
 
   it 'requests its current temperature' do
+    # Need to test that a call to antoher object is being made
+    # SETUP
+    #- there needs to be a weatherman.
+    weatherman = double :weatherman
+    #- there needs to be a temperature reporter - put the 'let' line in above.
+    new_york = CityTemperatureReporter.new('New York', weatherman)
+    # EXPECTATION ABOUT THE FUTURE
+    expect(weatherman).to receive(:get_temperature).with('New_York')
+    # this is the way to say receive a message with 'New_York' as a parameter
+
+    # ACTION
+    new_york.request_temperature
     # think about what TemperatureReporter needs to do its job
     # How can you test that it makes this request? Use diagrams to help you visualise this
   end
 
   it 'reports its current temperature' do
     # once it's got that temperature, can it report it?
+    #SETUP
+    # - need a weatherman double
+    weatherman = double :weatherman
+    seoul      = CityTemperatureReporter.new('Seoul', weatherman)
+    #EXPECTATION
+    expect(seoul.temperature).to eq '29'
   end
 end
